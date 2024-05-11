3d06b471ed1c144d2e72981f35ad3b3625386097
Allaberdiyev <149064535+Allaberdiyev@users.noreply.github.com>
2024-05-11 15:54:54 +0500

    movies

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:netflix_alternative_mobile_app/pages/details_page.dart';
+import 'package:netflix_alternative_mobile_app/pages/explore_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // home: ExplorePage(),
+        home: ExplorePage(),
+      ),
+    );
+  }
+}
